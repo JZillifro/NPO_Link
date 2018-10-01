5574bbd5b8253f92cce59f1535a34e6884218861
JZillifro <ZillifroJacob@gmail.com>
2018-09-30 20:52:16 -0500

    example router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+          <Redirect from='*' to='/404' status={404}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
       </div>
     );
   }
---
+++ frontend/src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {} from 'react-bootstrap';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        HOMEPAGE
+      </div>
+    )
+  }
+}
