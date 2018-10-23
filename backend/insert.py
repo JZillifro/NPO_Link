c2f743dc7db43d0feacaae2c4d148936e9e886f0
Gerardo Mares <23gmares@gmail.com>
2018-10-23 11:01:38 -0500

    changes to insert only data not already in db

--- backend/insert.py
+++ backend/insert.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import json
-
 from npolinkapi import app
 from npolinkapi import db
 from npolinkapi.location.models import Location
@@ -9,42 +8,29 @@ from npolinkapi.category.models import Category
 from npolinkapi.nonprofit.models import Nonprofit
 
 
-locations = []
 with open('files/data/loc-results.json') as json_data:
     data = json.load(json_data)
     for location in data:
-        loc = Location(id=location['id'],name=location['name'], city=location['city'], state=location['state'], description=location['description'], image=location['image'])
-        locations.append(loc)
+        exists = Location.query.filter_by(id=location['id']).first()
+        if not exists:
+            loc = Location(id=location['id'],name=location['name'], city=location['city'], state=location['state'], description=location['description'], image=location['image'])
+            db.session.add(loc)
 
-categories = []
 with open('files/data/cat-results.json') as json_data:
     data = json.load(json_data)
     for category in data:
-        cat = Category(id=category['id'],name=category['title'], description=location['description'], code=category['code'], image=category['image'], parent_category=category['parent_code'])
-        categories.append(cat)
+        exists = Category.query.filter_by(id=category['id']).first()
+        if not exists:
+            cat = Category(id=category['id'],name=category['title'], description=location['description'], code=category['code'], image=category['image'], parent_category=category['parent_code'])
+            db.session.add(cat)
 
-nonprofits = []
 with open('files/data/org-results.json') as json_data:
     data = json.load(json_data)
     for nonprofit in data:
-        org = Nonprofit(id=nonprofit['id'],name=nonprofit['name'], description=nonprofit['description'], ein=nonprofit['ein'], logo=nonprofit['logo'], address=nonprofit['address'], location_id=nonprofit['location_id'], category_id=nonprofit['category_id'])
-        nonprofits.append(org)
-
-for location in locations:
-    exists = Location.query.filter_by(id=location.id).first()
-    if not exists:
-        db.session.add(location)
-
-for category in categories:
-    exists = Category.query.filter_by(id=category.id).first()
-    if not exists:
-        db.session.add(category)
-
-for nonprofit in nonprofits:
-    exists = Nonprofit.query.filter_by(id=nonprofit.id).first()
-    if not exists:
-        db.session.add(nonprofit)
-
+        exists = Nonprofit.query.filter_by(id=nonprofit['id']).first()
+        if not exists:
+            org = Nonprofit(id=nonprofit['id'],name=nonprofit['name'], description=nonprofit['description'], ein=nonprofit['ein'], logo=nonprofit['logo'], address=nonprofit['address'], location_id=nonprofit['location_id'], category_id=nonprofit['category_id'])
+            db.session.add(org)
 
 db.session.flush()
 db.session.commit()
