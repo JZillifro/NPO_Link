aa255441d67643b61d51df3b1dbb9b754e388d37
Artsandwitchcraft <nakcs@utexas.edu>
2018-10-22 19:19:10 -0500

    Script to scrape wikipedia for city images

---
+++ backend/image.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def get_image():
+    base_url = 'https://en.wikipedia.org/api/rest_v1/page/media/'
+    with open("loc-results.json", 'r') as f:
+        locations =  json.load(f)
+        for loc in locations:
+            city = loc["city"]
+            state = loc["state"]
+            r = requests.get(base_url + city + ",_" + state)
+            try:
+                print(city)
+                print (r.json()["items"][0]["thumbnail"]["source"])
+            except:
+                print("Error finding image for %s, %s" % (city, state))
+if __name__ == "__main__":
+    get_image()
