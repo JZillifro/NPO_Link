a0ce780fc73192fd65c5852474096a55b778a10b
Gerardo Mares <23gmares@gmail.com>
2018-10-23 19:31:39 -0500

    changes to docker setup and refatroring project structure

--- backend/npolinkapi/__init__.py
+++ backend/npolinkapi/__init__.py
@@ -1,39 +1,43 @@
 import os
-# Import flask and template operators
-from flask import Flask, redirect, session, render_template
-from flask.cli import FlaskGroup
 
-# Import SQLAlchemy
+from flask import Flask  # new
 from flask_sqlalchemy import SQLAlchemy
 
-# Define the WSGI application object
-app = Flask(__name__)
 
-# Configurations
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
-# Define the database object which is imported
-# by modules and controllers
-db = SQLAlchemy(app)
-
-# Sample HTTP error handling
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
+# instantiate the db
+db = SQLAlchemy()
 
 
-# Import a module / component using its blueprint handler variable (mod_auth)
-from npolinkapi.location.controllers import locations_blueprint
-from npolinkapi.category.controllers import categories_blueprint
-from npolinkapi.nonprofit.controllers import nonprofits_blueprint
+# new
+def create_app(script_info=None):
 
-# Register blueprint(s)
-app.register_blueprint(locations_blueprint)
-app.register_blueprint(categories_blueprint)
-app.register_blueprint(nonprofits_blueprint)
+    # instantiate the app
+    app = Flask(__name__)
 
-@app.route("/")
-def greet():
-    #insert any documenation/behaviour for interaction with api at default endpoint here
-    return "Welcome to the NPO link api <Placeholder> <Placeholder> <Placeholder>\n"
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from npolinkapi.location.controllers import locations_blueprint
+    from npolinkapi.category.controllers import categories_blueprint
+    from npolinkapi.nonprofit.controllers import nonprofits_blueprint
+    # Register blueprint(s)
+    app.register_blueprint(locations_blueprint)
+    app.register_blueprint(categories_blueprint)
+    app.register_blueprint(nonprofits_blueprint)
+
+    @app.route("/")
+    def greet():
+        #insert any documenation/behaviour for interaction with api at default endpoint here
+        return "Welcome to the NPO link api <Placeholder> <Placeholder> <Placeholder>\n"
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
