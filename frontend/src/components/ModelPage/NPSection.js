bf2999430acb2100098336575dafa3b9b0f7ab1d
Gerardo Mares <23gmares@gmail.com>
2018-10-29 02:51:49 -0500

    started working on pagination

--- frontend/src/components/ModelPage/NPSection.js
+++ frontend/src/components/ModelPage/NPSection.js
@@ -1,59 +1,48 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import {PageHeader, Grid, Row, Col} from 'react-bootstrap';
 import ModelPanel from './ModelPanel.js';
 import NonProfitAPI from './../../api/NonProfitAPI';
 import LocationAPI from './../../api/LocationAPI';
 import CategoryAPI from './../../api/CategoryAPI';
 import axios from 'axios';
-import {BASE_API_URL} from './../constants.jsx';
-import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import { BASE_API_URL } from './../constants.jsx';
+
+import Pagination from "react-js-pagination";
 
 export default class NPSection extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      activePage: 1,
+      dataForPage : [],
+      totalNum: 12
+    }
   }
 
   componentDidMount() {
     axios.get(`${BASE_API_URL}/v1.0/${this.props.type}/1`).then(res => {
       const dataForPage = res.data.data.nonprofits
-      console.log(dataForPage)
-      const pages = res.data.pages
 
-      this.setState({pages, dataForPage})
+      this.setState({dataForPage: dataForPage, activePage: 1})
     }).catch(err => {
       console.log(err)
     });
   }
 
   changePage(i) {
-    axios.get(`${BASE_API_URL}/v1.0/${this.props.type}/${i}`).then(res => {
-      const dataForPage = res.data.data.nonprofits
-      console.log(dataForPage)
-      const pages = res.data.pages
+       axios.get(`${BASE_API_URL}/v1.0/${this.props.type}/${i}`).then(res => {
+         const dataForPage = res.data.data.nonprofits
 
-      this.setState({pages, dataForPage})
-    }).catch(err => {
-      console.log(err)
-    });
+         this.setState({activePage: i, dataForPage: dataForPage})
+         window.scrollTo(0, 0)
+       }).catch(err => {
+         console.log(err)
+       });
   }
 
   render() {
-    // console.log("this.state.models")
-    // console.log(this.state.models[this.props.type])
-    var pagelinks = []
-    for(var i = 1; i <= this.state.pages; i++) {
-      const page = i;
-      pagelinks.push(
-        <PaginationItem>
-          <PaginationLink onClick={() => this.changePage(page)}>
-            {page}
-          </PaginationLink>
-        </PaginationItem>
-      );
-    }
-    console.log(pagelinks)
-    if(this.state.pages && this.state.dataForPage) {
+    if(this.state.dataForPage) {
       return(
         <div>
           <div className="row">
@@ -80,13 +69,12 @@ export default class NPSection extends React.Component {
             }
           </div>
           <br/>
-          <Pagination aria-label="Page navigation example">
-            {
-              pagelinks.map((link) => {
-                return(link)
-              })
-            }
-          </Pagination>
+
+          <Pagination pageRangeDisplayed={10} activePage={this.state.activePage}
+                      activeLinkClass = "active" totalItemsCount={this.state.totalNum}
+                      itemsCountPerPage={12}
+                      onChange={this.changePage} />
+
         </div>
       );
     } else {
