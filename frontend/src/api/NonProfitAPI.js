3371a5eba79811953e9d636d282c5cd0a35b4ca2
Artsandwitchcraft <nakcs@utexas.edu>
2018-10-30 19:32:30 -0500

    Nonprofits working

--- frontend/src/api/NonProfitAPI.js
+++ frontend/src/api/NonProfitAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // A simple data API that will be used to get the data for our
 // components. On a real website, a more robust data fetching
 // solution would be more appropriate.
-export async function getLocation(id) {
+export async function getNonProfit(id) {
   const response = axios.get(`${BASE_API_URL}/v1.0/nonprofits/nonprofit/${id}`)
   return response
 }
