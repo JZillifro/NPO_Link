03c4c22cf9253a65f027d3f1fa637d4542c7253b
Artsandwitchcraft <nakcs@utexas.edu>
2018-10-30 19:51:51 -0500

    Setup to get mocha woring

--- frontend/src/components/constants.jsx
+++ frontend/src/components/constants.jsx
@@ -1,5 +1,5 @@
-//const BASE_API_URL = 'http://npolink.me:5000'
-const BASE_API_URL = 'http://127.0.0.1:5000'
+const BASE_API_URL = 'http://api.npolink.me'
+//const BASE_API_URL = 'http://127.0.0.1:5000'
 export {
  BASE_API_URL
 }
