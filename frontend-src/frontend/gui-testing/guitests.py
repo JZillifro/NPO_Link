7ac154995869758e6b381bcf09cd96221f4395fb
Georgina Garza <georginagarza@wireless-10-145-253-222.public.utexas.edu>
2018-11-13 18:32:20 -0600

    Final GUI tests

--- frontend-src/frontend/gui-testing/guitests.py
+++ frontend-src/frontend/gui-testing/guitests.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class AcceptanceTests (TestCase):
 
 	def setUp(self):
-		self.homeurl = 'http://localhost/all/search'
+		self.homeurl = 'http://npolink.me'
 		if platform == "linux" or platform == "linux2":
 			self.browser = webdriver.Chrome('./chrome_drivers/chromedriver_linux')
 		elif platform == "darwin": # macOS
@@ -19,99 +19,99 @@ class AcceptanceTests (TestCase):
 			self.browser = webdriver.Chrome('./chrome_drivers/chromedriver.exe')
 		self.browser.get(self.homeurl)
 
-	# # Tests that the browser title for the homepage is correct
-	# def test1(self):
-	# 	self.browser.get(self.homeurl)
-	# 	self.assertIn('NPOLink', self.browser.title)
-	#
-	# # Tests that the home page component on navigation bar works
-	# def test2(self):
-	# 	try:
-	# 		self.browser.find_element_by_link_text('Home').click()
-	# 		self.assertEqual("http://npolink.me/", self.browser.current_url)
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
-	#
-	# # Tests that the homepage header is correct
-	# def test3(self):
-	# 	self.browser.find_element_by_link_text('Home').click()
-	# 	given = self.browser.find_element_by_class_name("inner").text
-	# 	self.assertIn('NPOLink', given)
-	#
-	# # Tests that nonprofits component on navigation bar works
-	# def test4(self):
-	# 	try:
-	# 		self.browser.find_element_by_link_text('Nonprofits').click()
-	# 		self.assertEqual("http://npolink.me/nonprofits", self.browser.current_url)
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
-	#
-	# # Tests that the nonprofits header is correct
-	# def test5(self):
-	# 	self.browser.find_element_by_link_text('Nonprofits').click()
-	# 	given = self.browser.find_element_by_class_name("container").text
-	# 	self.assertIn('Nonprofits', given)
-	#
-	# # Tests that categories component on navigation bar works
-	# def test6(self):
-	# 	try:
-	# 		self.browser.find_element_by_link_text('Categories').click()
-	# 		self.assertEqual("http://npolink.me/categories", self.browser.current_url)
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
-	#
-	# # Tests that the categories header is correct
-	# def test7(self):
-	# 	self.browser.find_element_by_link_text('Categories').click()
-	# 	given = self.browser.find_element_by_class_name("container").text
-	# 	self.assertIn('Categories', given)
-	#
-	# # Tests that locations component on navigation bar works
-	# def test8(self):
-	# 	try:
-	# 		self.browser.find_element_by_link_text('Locations').click()
-	# 		self.assertEqual("http://npolink.me/locations", self.browser.current_url)
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
-	#
-	# # Tests that the locations header is correct
-	# def test9(self):
-	# 	self.browser.find_element_by_link_text('Locations').click()
-	# 	given = self.browser.find_element_by_class_name("container").text
-	# 	self.assertIn('Locations', given)
-	#
-	# # Tests that about component on navigation bar works
-	# def test10(self):
-	# 	try:
-	# 		self.browser.find_element_by_link_text('About').click()
-	# 		self.assertEqual("http://npolink.me/about", self.browser.current_url)
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
-	#
-	# # Tests that the about header is correct
-	# def test11(self):
-	# 	self.browser.find_element_by_link_text('About').click()
-	# 	given = self.browser.find_element_by_class_name("container").text
-	# 	self.assertIn('About', given)
-	#
-	# # Tests that link to gitlab repository works
-	# def test12(self):
-	# 	self.browser.find_element_by_link_text('About').click()
-	# 	try:
-	# 		self.browser.find_element_by_link_text('GitLab Repository').click()
-	# 		self.assertEqual("https://gitlab.com/gerardomares/npolink", self.browser.current_url)
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
-	#
-	# # Tests added for Phase 3
-	#
-	# # Make sure can get to universal search page
-	# def test14(self):
-	# 	try:
-	# 		self.browser.find_element_by_link_text('Search').click()
-	# 		self.assertEqual("http://npolink.me/all/search", self.browser.current_url)
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
+	# Tests that the browser title for the homepage is correct
+	def test1(self):
+		self.browser.get(self.homeurl)
+		self.assertIn('NPOLink', self.browser.title)
+
+	# Tests that the home page component on navigation bar works
+	def test2(self):
+		try:
+			self.browser.find_element_by_link_text('Home').click()
+			self.assertEqual("http://npolink.me/", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
+
+	# Tests that the homepage header is correct
+	def test3(self):
+		self.browser.find_element_by_link_text('Home').click()
+		given = self.browser.find_element_by_class_name("inner").text
+		self.assertIn('NPOLink', given)
+
+	# Tests that nonprofits component on navigation bar works
+	def test4(self):
+		try:
+			self.browser.find_element_by_link_text('Nonprofits').click()
+			self.assertEqual("http://npolink.me/nonprofits", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
+
+	# Tests that the nonprofits header is correct
+	def test5(self):
+		self.browser.find_element_by_link_text('Nonprofits').click()
+		given = self.browser.find_element_by_class_name("container").text
+		self.assertIn('Nonprofits', given)
+
+	# Tests that categories component on navigation bar works
+	def test6(self):
+		try:
+			self.browser.find_element_by_link_text('Categories').click()
+			self.assertEqual("http://npolink.me/categories", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
+
+	# Tests that the categories header is correct
+	def test7(self):
+		self.browser.find_element_by_link_text('Categories').click()
+		given = self.browser.find_element_by_class_name("container").text
+		self.assertIn('Categories', given)
+
+	# Tests that locations component on navigation bar works
+	def test8(self):
+		try:
+			self.browser.find_element_by_link_text('Locations').click()
+			self.assertEqual("http://npolink.me/locations", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
+
+	# Tests that the locations header is correct
+	def test9(self):
+		self.browser.find_element_by_link_text('Locations').click()
+		given = self.browser.find_element_by_class_name("container").text
+		self.assertIn('Locations', given)
+
+	# Tests that about component on navigation bar works
+	def test10(self):
+		try:
+			self.browser.find_element_by_link_text('About').click()
+			self.assertEqual("http://npolink.me/about", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
+
+	# Tests that the about header is correct
+	def test11(self):
+		self.browser.find_element_by_link_text('About').click()
+		given = self.browser.find_element_by_class_name("container").text
+		self.assertIn('About', given)
+
+	# Tests that link to gitlab repository works
+	def test12(self):
+		self.browser.find_element_by_link_text('About').click()
+		try:
+			self.browser.find_element_by_link_text('GitLab Repository').click()
+			self.assertEqual("https://gitlab.com/gerardomares/npolink", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
+
+	# Tests added for Phase 3
+
+	# Make sure can get to universal search page
+	def test14(self):
+		try:
+			self.browser.find_element_by_link_text('Search').click()
+			self.assertEqual("http://npolink.me/all/search", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
 
 	# Tests the overall search for the site
 	def test13(self):
@@ -129,7 +129,7 @@ class AcceptanceTests (TestCase):
 
 		try:
 			self.browser.find_element_by_id('Pollution Abatement & Control').click()
-			self.assertEqual("http://localhost/category/83", self.browser.current_url)
+			self.assertEqual("http://npolink.me/category/83", self.browser.current_url)
 		except NoSuchElementException:
 			self.fail("The element doesn't exist")
 
@@ -145,27 +145,27 @@ class AcceptanceTests (TestCase):
 
 		try:
 			self.browser.find_element_by_id('Community Water Solutions').click()
-			self.assertEqual("http://localhost/nonprofit/132", self.browser.current_url)
+			self.assertEqual("http://npolink.me/nonprofit/132", self.browser.current_url)
 		except NoSuchElementException:
 			self.fail("The element doesn't exist")
 
-	# # Tests the filtering for the Nonprofits page
-	# def test15(self):
-	# 	self.browser.find_element_by_link_text('Nonprofits').click()
-	#
-	# 	filter_button = self.browser.find_element_by_id("filter_bttn") # update word "filter" once implemented
-	# 	filter_button.click()
-	#
-	# 	# might need a time/sleep here
-	# 	selection = self.browser.find_element_by_id("selection") # update word "selection" once implemented
-	# 	selection.click()
-	#
-	# 	# might need a time/sleep here
-	# 	try:
-	# 		self.browser.find_element_by_link_text('Nonprofit Name').click() # update word "specific_select" once implemented
-	# 		self.assertEqual("http://localhost/nonprofit/", self.browser.current_url) # update expected url once implemented
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
+	# Tests the filtering for the Nonprofits page
+	def test15(self):
+		self.browser.find_element_by_link_text('Nonprofits').click()
+
+		filter_button = self.browser.find_element_by_id("filter_key_dropdown") # for state
+		filter_button.click()
+
+		# might need a time/sleep here
+		selection = self.browser.find_element_by_id("selection_CA")
+		selection.click()
+
+		# might need a time/sleep here
+		try:
+			self.browser.find_element_by_id('A Home Within').click()
+			self.assertEqual("http://npolink.me/nonprofit/159", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
 
 	# Tests the sorting for the Nonprofits page
 	def test16(self):
@@ -181,7 +181,7 @@ class AcceptanceTests (TestCase):
 		# might need a time/sleep here
 		try:
 			self.browser.find_element_by_id('3 Generations').click()
-			self.assertEqual("http://localhost/nonprofit/121", self.browser.current_url)
+			self.assertEqual("http://npolink.me/nonprofit/121", self.browser.current_url)
 		except NoSuchElementException:
 			self.fail("The element doesn't exist")
 
@@ -197,27 +197,27 @@ class AcceptanceTests (TestCase):
 
 		try:
 			self.browser.find_element_by_id('Watertown, MA').click()
-			self.assertEqual("http://localhost/location/51", self.browser.current_url)
+			self.assertEqual("http://npolink.me/location/51", self.browser.current_url)
 		except NoSuchElementException:
 			self.fail("The element doesn't exist")
 
-	# # Tests the filtering for the Locations page
-	# def test18(self):
-	# 	self.browser.find_element_by_link_text('Locations').click()
-	#
-	# 	filter_button = self.browser.find_element_by_id("filter_bttn") # update word "filter" once implemented
-	# 	filter_button.click()
-	#
-	# 	# might need a time/sleep here
-	# 	selection = self.browser.find_element_by_id("selection") # update word "selection" once implemented
-	# 	selection.click()
-	#
-	# 	# might need a time/sleep here
-	# 	try:
-	# 		self.browser.find_element_by_link_text('Location Name').click() # update word "keywords" once implemented
-	# 		self.assertEqual("http://localhost/location/", self.browser.current_url) # update expected url once implemented
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
+	# Tests the filtering for the Locations page
+	def test18(self):
+		self.browser.find_element_by_link_text('Locations').click()
+
+		filter_button = self.browser.find_element_by_id("filter_key_dropdown") # for state
+		filter_button.click()
+
+		# might need a time/sleep here
+		selection = self.browser.find_element_by_id("selection_CA")
+		selection.click()
+
+		# might need a time/sleep here
+		try:
+			self.browser.find_element_by_id('Angwin, CA').click()
+			self.assertEqual("http://npolink.me/location/49", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
 
 	# Tests the sorting for the Locations page
 	def test19(self):
@@ -233,7 +233,7 @@ class AcceptanceTests (TestCase):
 		# might need a time/sleep here
 		try:
 			self.browser.find_element_by_id('Alexandria, VA').click()
-			self.assertEqual("http://localhost/location/43", self.browser.current_url)
+			self.assertEqual("http://npolink.me/location/43", self.browser.current_url)
 		except NoSuchElementException:
 			self.fail("The element doesn't exist")
 
@@ -249,27 +249,27 @@ class AcceptanceTests (TestCase):
 
 		try:
 			self.browser.find_element_by_id('Swimming & Other Water Recreation').click()
-			self.assertEqual("http://localhost/category/362", self.browser.current_url)
+			self.assertEqual("http://npolink.me/category/362", self.browser.current_url)
 		except NoSuchElementException:
 			self.fail("The element doesn't exist")
 
-	# # Tests the filtering for the Categories page
-	# def test21(self):
-	# 	self.browser.find_element_by_link_text('Categories').click()
-	#
-	# 	filter_button = self.browser.find_element_by_id("filter_bttn") # update word "filter" once implemented
-	# 	filter_button.click()
-	#
-	# 	# might need a time/sleep here
-	# 	selection = self.browser.find_element_by_id("selection") # update word "selection" once implemented
-	# 	selection.click()
-	#
-	# 	# might need a time/sleep here
-	# 	try:
-	# 		self.browser.find_element_by_link_text("Category Name").click() # update word "keywords" once implemented
-	# 		self.assertEqual("http://localhost/category/", self.browser.current_url) # update expected url once implemented
-	# 	except NoSuchElementException:
-	# 		self.fail("The element doesn't exist")
+	# Tests the filtering for the Categories page
+	def test21(self):
+		self.browser.find_element_by_link_text('Categories').click()
+
+		filter_button = self.browser.find_element_by_id("filter_key_dropdown") #for parent category code
+		filter_button.click()
+
+		# might need a time/sleep here
+		selection = self.browser.find_element_by_id("selection_G")
+		selection.click()
+
+		# might need a time/sleep here
+		try:
+			self.browser.find_element_by_id("AIDS").click()
+			self.assertEqual("http://npolink.me/category/195", self.browser.current_url)
+		except NoSuchElementException:
+			self.fail("The element doesn't exist")
 
 	# Tests the sorting for the Categories page
 	def test22(self):
@@ -285,7 +285,7 @@ class AcceptanceTests (TestCase):
 		# might need a time/sleep here
 		try:
 			self.browser.find_element_by_id("AIDS").click()
-			self.assertEqual("http://localhost/category/195", self.browser.current_url)
+			self.assertEqual("http://npolink.me/category/195", self.browser.current_url)
 		except NoSuchElementException:
 			self.fail("The element doesn't exist")
 
